bf5196780dcaab3d095e8fa5a90911ceb146bd0a
Donald Okonkwo <chibuezeokon1007@gmail.com>
2025-11-28 09:14:15 +0100

    removed the navigation method and its locator into the tinder_automation.py

--- login_page.py
+++ login_page.py
@@ -1,66 +1,32 @@
 import os
-from dotenv import load_dotenv
-from selenium import webdriver
-from selenium.common import TimeoutException, NoSuchElementException, StaleElementReferenceException
-from selenium.webdriver import ChromeOptions
+from selenium.common import TimeoutException
 import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import logging
 
 from base_page import BasePage
 
+
 class LoginPage(BasePage):
     def __init__(self, driver, logger):
         super().__init__(driver, logger)
-        self.WEB_URL = "https://tinder.com"
         self.current_web_title = self.driver.title.lower()
 
         #credentials
         self.username = os.getenv("YOUR_USERNAME")
         self.password = os.getenv("YOUR_PASSWORD")
 
-
         self.fb_login_title = 'Facebook'
         self.tinder_title = 'Tinder | Match. Chat. Date.'
 
         #locators
-        self.NAVIGATE_TO_LOGIN_BTN = (By.XPATH, '//div[text()="Log in"]')
-        self.FB_1ST_LOGIN_BTN_LOCATOR = (By.XPATH, '//div[text()="Log in with Facebook"]')
+
         self.FB_LOGIN_BTN_LOCATOR = (By.NAME, 'login')
         self.EMAIL_LOCATOR = (By.ID, 'email')
         self.PASSWORD_LOCATOR = (By.ID, 'pass')
         self.TEXT_ON_FB_PAGE = (By.ID, "homelink")
 
-    def click_tinder_login(self):
-        tinder_login_element = self.wait.until(
-            ec.presence_of_element_located(self.NAVIGATE_TO_LOGIN_BTN)
-        )
-        tinder_login_element.click()
-
-    def click_tinder_fb_login_btn(self):
-        fb_login_element = self.wait.until(
-            ec.presence_of_element_located(self.FB_1ST_LOGIN_BTN_LOCATOR)
-        )
-        fb_login_element.click()
-
-    def navigate_to_login(self):
-        self.driver.get(self.WEB_URL)
-
-        try:
-            self.click_tinder_login()
-            self.click_tinder_fb_login_btn()
-            return True
-
-        except TimeoutException:
-            logging.warning("Timed out while navigating to login page")
-            return False
-
-        except StaleElementReferenceException:
-            logging.warning("Login button not fully loaded while navigating to login page")
-            return False
 
     def click_fb_login_btn(self):
         try:
@@ -104,8 +70,7 @@ class LoginPage(BasePage):
             logging.info(f'Failed to switch to {new_window_title}, page still in {self.current_web_title}')
             raise ValueError(f"Failed to switch to {new_window_title},page still in {self.current_web_title}")
 
-    def login_fb(self):
-        is_navigate_login = self.navigate_to_login()
+    def login_fb(self,is_navigate_login):
 
         if not is_navigate_login:
             raise ValueError('Login failed due to timeout while navigating to login page')
